74ccc7b658140bd91f47b1d1e2be85d49508c7ad
NickanBagherpour <Nickan.bagherpour@gmail.com>
2022-10-06 15:53:59 +0330

    feat: implement header,footer, update primary layout,index(home), remove unused files, ...

--- components/templates/base/BaseTemplate.tsx
+++ components/templates/base/BaseTemplate.tsx
@@ -5,15 +5,7 @@ export interface IBaseTemplate {
 }
 
 const BaseTemplate: React.FC<IBaseTemplate> = ({ sampleTextProp }) => {
-  return (
-    <div
-      className={
-        styles.container /* 'relative flex min-h-screen flex-col justify-center overflow-hidden bg-gray-50 py-6 sm:py-12' */
-      }
-    >
-      {sampleTextProp}
-    </div>
-  );
+  return <div className={styles.container}>{sampleTextProp}</div>;
 };
 
 export default BaseTemplate;
